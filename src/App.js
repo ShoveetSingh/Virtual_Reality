f290f9e2deb1600498df667aeb498ab89d4e4708
@Shoveet2003 <shoveetsingh2002@gmail.com>
2024-11-22 17:16:03 +0530

    background scene created!!!

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import 'aframe' 
+import 'aframe' ;
+import 'aframe-environment-component';
 
 function App() {
   return (
     <a-scene>
+      <a-box 
+      color="tomato" 
+      position="0 2 -5"
+      rotation="0 45 0"
+      scale="2 2 2"
+      width="0.4" height="0.3" depth="0.1"
+      animation="
+      property:object3D.position.x; 
+      to: -2.2; dir:reverse;
+      dur:2500; loop:true;"
+      >
+      </a-box>
+      <a-entity
+       environment="preset: starry; dressingAmount: 500 sky:purple">
+      </a-entity>
     </a-scene>
   );
 }
